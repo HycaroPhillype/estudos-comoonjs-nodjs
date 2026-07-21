8e8fd190e6d52f35ea6dc716bdcd0fc87a499e30
Hycaro Phillype <phillypehycaro@gmail.com>
2026-07-21 13:56:14 -0300

    iniciando estudos de modularização Node.js

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const products = require("./service/products");
+
+async function main() {
+  console.log("PATO");
+  products.getFullName("556", "hycaro");
+}
+
+main();
---
+++ src/service/products.js
@@ -0,0 +1,9 @@
+async function getFullName(codeId, productName) {
+  console.log("\n");
+
+  console.log("Produto: " + codeId + "----" + productName);
+}
+
+module.exports = {
+  getFullName,
+};
