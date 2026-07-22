402c343a43f705ad7ae21559ec21ce9397a8b261
Hycaro Phillype <phillypehycaro@gmail.com>
2026-07-21 22:34:47 -0300

    Destructuring

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-const products = require("./service/products");
+const { getFullName, getProductLabel } = require("./service/products");
 const config1 = require("./service/config");
 const database = require("./service/database");
 
 async function main() {
   console.log("PATO");
-  products.getFullName("556", "hycaro");
-  products.getProductLabel("Miojo");
+  getFullName("556", "hycaro");
+  getProductLabel("Miojo");
   console.log(config1.production);
   console.log(database.connectToDatabase);
 }
